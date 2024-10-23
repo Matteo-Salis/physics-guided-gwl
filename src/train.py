9ff671509fd6f758451153a0e88746f37184a971
gabrielesartor <sartor.gabriele23@gmail.com>
2024-10-23 19:01:41 +0200

    First commit

---
+++ src/train.py
@@ -0,0 +1,13 @@
+import os
+import torch
+import time
+import logging
+import json
+import wandb
+import argparse
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+wandb.login()
+
+if __name__ == "__main__":
+    pass
