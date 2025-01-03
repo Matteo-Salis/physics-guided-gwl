piezo head, normalization and fixes 1d continuous case

src/dataloaders/load_1d_meteo_wtd.py
@@ -52,25 +52,14 @@ class ContinuousDataset(Dataset):
         
         if dict_files["normalization"] is True:
             
-            self.norm_factors(date_max = np.datetime64(dict_files["date_max_norm"]))
-            
-            self.wtd_df[self.target] = (self.wtd_df[self.target] - self.self.target_mean)/self.self.target_std
-            
-            self.wtd_df["lat"] = (self.wtd_df["lat"] - self.lat_mean)/self.lat_std
-            self.wtd_df["lon"] = (self.wtd_df["lon"] - self.lon_mean)/self.lon_std
-            
-            self.dtm_roi = (self.dtm_roi - self.dtm_mean)/self.dtm_std
-            self.weather_dtm = (self.weather_dtm - self.dtm_mean.values)/self.dtm_std.values
-            
-            self.weather_coords[:,:,0] = (self.weather_coords[:,:,0] - self.lat_mean)/self.lat_std
-            self.weather_coords[:,:,1] = (self.weather_coords[:,:,1] - self.lon_mean)/self.lon_std
-            
-            self.weather_xr = (self.weather_xr - self.weather_mean)/self.weather_std
+            self.normalize(date_max = np.datetime64(dict_files["date_max_norm"]))
 
         # Transform       
         self.transform = dict_files["transform"]
         
-    def compute_norm_factors(self, date_max = np.datetime64("2020-01-01"), verbose = False, dict_out = False):
+        
+    def compute_norm_factors(self, date_max = np.datetime64("2020-01-01"), verbose = True, dict_out = False):
+        
         subset_wtd_df = self.wtd_df.loc[pd.IndexSlice[self.wtd_df.index.get_level_values(0) <= date_max,
                                                        :]]
         subset_weather_xr = self.weather_xr.sel(time = slice(date_max)) #slice include extremes
@@ -100,20 +89,33 @@ class ContinuousDataset(Dataset):
         if verbose is True:
             print("Norm factors:")
             print(self.norm_factors)
+            print(f"Max date norm: {date_max}")
             
         if dict_out is True:
             return self.norm_factors
         
-    def normalize(self, norm_factors = None):
+    def normalize(self, norm_factors = None, date_max = np.datetime64("2020-01-01")):
         if norm_factors is None:
-            self.compute_norm_factors()
+            self.compute_norm_factors(date_max = date_max)
             ## compute norm factors by default
             
-        # else: if n.f are provided
+        else:
+            ## if provided 
+            self.norm_factors = norm_factors
         
-        # compute norm
-        pass            
-        
+        # Normalizations
+        self.wtd_df[self.target] = (self.wtd_df[self.target] - self.norm_factors["target_mean"])/self.norm_factors["target_std"]
+            
+        self.wtd_df["lat"] = (self.wtd_df["lat"] - self.norm_factors["lat_mean"])/self.norm_factors["lat_std"]
+        self.wtd_df["lon"] = (self.wtd_df["lon"] - self.norm_factors["lon_mean"])/self.norm_factors["lon_std"]
+            
+        self.dtm_roi = (self.dtm_roi - self.norm_factors["dtm_mean"])/self.norm_factors["dtm_std"]
+        self.weather_dtm = (self.weather_dtm - self.norm_factors["dtm_mean"].values)/self.norm_factors["dtm_std"].values
+            
+        self.weather_coords[:,:,0] = (self.weather_coords[:,:,0] - self.norm_factors["lat_mean"])/self.norm_factors["lat_std"]
+        self.weather_coords[:,:,1] = (self.weather_coords[:,:,1] - self.norm_factors["lon_mean"])/self.norm_factors["lon_std"]
+            
+        self.weather_xr = (self.weather_xr - self.norm_factors["weather_mean"])/self.norm_factors["weather_std"]       
             
         
     def loading_dtm(self):
@@ -190,6 +192,9 @@ class ContinuousDataset(Dataset):
         
         if fill_value:
             self.wtd_df["wtd"] = self.wtd_df["wtd"].fillna(fill_value)
+            self.fill_value = fill_value
+        else:
+            self.fill_value = 0
         
     def compute_piezo_head(self):
         heights = []
@@ -207,6 +212,10 @@ class ContinuousDataset(Dataset):
     
     
         self.wtd_names["height"] = np.array(heights)
+        
+    def get_iloc_from_date(self, date_max):
+        row_num = self.wtd_df.index.get_loc(self.wtd_df[self.wtd_df.index.get_level_values(0) < date_max].iloc[-1].name)
+        return row_num
         
     def __len__(self):
         data = self.wtd_df.loc[pd.IndexSlice[self.wtd_df.index.get_level_values(0) <= self.input_dates.max(),
@@ -245,7 +254,7 @@ class ContinuousDataset(Dataset):
         X = [torch.from_numpy(target_t0_lat).to(torch.float32),
              torch.from_numpy(target_t0_lon).to(torch.float32),
              torch.from_numpy(target_t0_dtm).to(torch.float32),
-             torch.from_numpy(target_t0_values).to(torch.float32)
+             torch.from_numpy(target_t0_values).to(torch.float32).nan_to_num(self.fill_value)
              ]
         X = torch.stack(X, dim = -1)
         

src/models/load_models_1d.py
@@ -16,7 +16,8 @@ class Continuous1DNN(nn.Module):
                  conv_filters = 32,
                  lstm_layer = 5,
                  lstm_input_units = 16,
-                 lstm_units = 32):
+                 lstm_units = 32,
+                 ):
         super().__init__()
         
         self.timestep = timestep
@@ -27,7 +28,7 @@ class Continuous1DNN(nn.Module):
         self.cb_fc_neurons = cb_fc_neurons
         self.conv_filters = conv_filters
         
-        self.wgamma = nn.Sigmoid()
+        self.wgamma = nn.Softmax(dim = -1)
         # Fully connected
         cb_fc = []
         cb_fc.append(nn.Linear(4, self.cb_fc_neurons))
@@ -41,10 +42,10 @@ class Continuous1DNN(nn.Module):
         self.cb_fc = nn.Sequential(*cb_fc)
         
         # Weather block
-        self.weather_wgamma = nn.Sigmoid()
+        #self.weather_wgamma = nn.Softmax(dim = -1)
         
         conv3d_stack=[]
-        conv3d_stack.append(nn.Conv3d(10, self.conv_filters, (1,2,2))) # Conv input (N, C, D, H, W) - kernel 3d (D, H, W)
+        conv3d_stack.append(nn.Conv3d(14, self.conv_filters, (1,2,2))) # Conv input (N, C, D, H, W) - kernel 3d (D, H, W)
         conv3d_stack.append(nn.BatchNorm3d(self.conv_filters))
         conv3d_stack.append(nn.ReLU())
         
@@ -87,23 +88,27 @@ class Continuous1DNN(nn.Module):
         
         # Conditioning block
         
-        wtd_sim = torch.matmul(x[:,:,:3], z.unsqueeze(-1))
-        wtd_sim = wtd_sim/torch.sqrt(torch.tensor(z.shape[-1])) 
-        wtd_sim = self.wgamma(wtd_sim)
-        wtd_sim = wtd_sim.squeeze() * x_mask # masking nan
-        wtd0 = torch.sum(x[:,:,-2] * wtd_sim, dim = 1)/torch.sum(wtd_sim, dim = 1)
-        wtd0 = torch.cat([z, wtd0.unsqueeze(-1)], dim = -1)
+        target_sim = torch.matmul(x[:,:,:3], z.unsqueeze(-1))
+        target_sim = target_sim/torch.sqrt(torch.tensor(z.shape[-1])) 
+        target_sim = self.wgamma(target_sim.squeeze())
+        target_sim = target_sim * x_mask # masking nan
+        target0 = torch.sum(x[:,:,-1] * target_sim, dim = 1)/torch.sum(target_sim, dim = 1)
+        target0 = torch.cat([z, target0.unsqueeze(-1)], dim = -1)
         
-        wtd0 = self.cb_fc(wtd0)
+        target0 = self.cb_fc(target0)
         
         # Weather block
         ## w[0] (10, 180, 9, 12); w[1] (9, 12, 3)
         weather_sim = w[1] * z[:,None,None,:].expand(-1, w[1].shape[1], w[1].shape[2], -1)
         weather_sim = torch.sum(weather_sim, dim = -1)/torch.sqrt(torch.tensor(weather_sim.shape[-1]))
-        weather_sim = self.weather_wgamma(weather_sim)
-        weather_sim = weather_sim[:, None, None, : ,:].expand(-1, w[0].shape[1], w[0].shape[2], -1, -1 )
+        #weather_sim = self.weather_wgamma(weather_sim)
         
-        weather = w[0] * weather_sim
+        weather_sim = weather_sim[:, None, None, : ,:].expand(-1, -1, w[0].shape[2], -1, -1 )
+        
+        
+        weather = torch.cat([w[0],
+                             torch.moveaxis(w[1], -1, 1).unsqueeze(2).expand(-1, -1, w[0].shape[2], -1, -1 ) ,
+                             weather_sim], dim = 1)
         
         wb_td3dconv = self.conv3d_stack(weather)
         
@@ -111,19 +116,19 @@ class Continuous1DNN(nn.Module):
         wb_td3dconv = torch.moveaxis(wb_td3dconv, 1, -1)
         
         # Sequential block
-        wtd0_h = wtd0.unsqueeze(1).expand([-1,self.lstm_layer,-1])
-        wtd0_h = nn.Tanh()(torch.movedim(wtd0_h, 0, 1)) 
+        target0_h = target0.unsqueeze(1).expand([-1,self.lstm_layer,-1])
+        target0_h = nn.Tanh()(torch.movedim(target0_h, 0, 1)) 
         
-        wtd_ts = self.lstm_1(wb_td3dconv,
-                                 (wtd0_h.contiguous(),
-                                  torch.zeros_like(wtd0_h))) #input  [input, (h_0, c_0)] - h and c (D∗num_layers,N,H)
+        target_ts = self.lstm_1(wb_td3dconv,
+                                 (target0_h.contiguous(),
+                                  torch.zeros_like(target0_h))) #input  [input, (h_0, c_0)] - h and c (D∗num_layers,N,H)
         
-        wtd0_skip = wtd0.unsqueeze(1).expand([-1,self.timestep,-1])
-        wtd_ts_out = wtd_ts[0] + wtd0_skip
+        target0_skip = target0.unsqueeze(1).expand([-1,self.timestep,-1])
+        target_ts_out = target_ts[0] + target0_skip
         
-        wtd_ts = self.fc(wtd_ts_out)
+        target_ts_out = self.fc(target_ts_out)
         
-        return wtd_ts.squeeze()
+        return target_ts_out.squeeze()
     
 
 ############## MODEL 2 ##############

src/notebooks/test_1d_script.py
@@ -60,7 +60,7 @@ print(f"Length of the dataset: {ds.__len__()}")
 
 # %%
 device = (
-    "cuda"
+    "cuda:" + str(dict_files["gpu_idx"])
     if torch.cuda.is_available()
     else "mps"
     if torch.backends.mps.is_available()
@@ -86,25 +86,35 @@ print("Total number of trainable parameters: " ,sum(p.numel() for p in model.par
 batch_size = dict_files["batch_size"]
 max_epochs = dict_files["epochs"]
 
-test_split_p = dict_files["test_split_p"]
-train_split_p = 1 - test_split_p
-
 max_ds_elems = ds.__len__()
 if not dict_files["all_dataset"]:
     max_ds_elems = dict_files["max_ds_elems"]
+    
+if type(dict_files["test_split_p"]) is str:
+    
+    train_idx = int(ds.get_iloc_from_date(date_max= np.datetime64(dict_files["test_split_p"])))
+    test_idx = int(max_ds_elems - train_idx)
+else:
+    test_split_p = dict_files["test_split_p"]
+    train_split_p = 1 - test_split_p
+    
+    train_idx = int(max_ds_elems*train_split_p)
+    test_idx = int(max_ds_elems*test_split_p)
 
-train_idx = int(max_ds_elems*train_split_p)
-test_idx = int(max_ds_elems*test_split_p)
-#offset_idx = 100000
-
-print(f"Traing size: {train_idx}, Test size: {test_idx}")
-#offset_idx+
 train_idxs, test_idxs = np.arange(train_idx), np.arange(train_idx,
                                                         train_idx + test_idx)
 
-train_sampler = RandomSampler(train_idxs)
-test_sampler = RandomSampler(test_idxs)
+print(f"Traing size: {train_idx} - {ds.wtd_df.index.get_level_values(0)[train_idxs[-1]]}, Test size: {test_idx} - {ds.wtd_df.index.get_level_values(0)[test_idxs[-1]]}")
 
+# Sampler 
+if dict_files["random_sampler"] is True:
+    train_sampler = RandomSampler(train_idxs)
+else:
+    train_sampler = SequentialSampler(train_idxs)
+    
+test_sampler = SequentialSampler(test_idxs)
+
+# DataLoaders
 train_loader = torch.utils.data.DataLoader(dataset=ds,
                                             batch_size=batch_size,
                                             sampler=train_sampler)
@@ -160,7 +170,7 @@ weather_dtm = ds.get_weather_dtm()
 weather_coords = torch.cat([weather_coords, weather_dtm], dim = -1)
 
 print('mem allocated in MB: ', torch.cuda.memory_allocated() / 1024**2)
-print(torch.cuda.memory_summary(device=None, abbreviated=False))
+print(torch.cuda.memory_summary(device=device, abbreviated=False))
 
 for i in range(max_epochs):
     