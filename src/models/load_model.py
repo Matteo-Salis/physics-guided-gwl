b838848a82a7470794851739e75462ac1c418bda
gabrielesartor <sartor.gabriele23@gmail.com>
2025-01-14 10:04:16 +0100

    Updated 2D models in load_model.py

--- src/models/load_model.py
+++ src/models/load_model.py
@@ -4,9 +4,13 @@ from models.load_models_2d import *
 def load_model(config):
     timesteps = config["timesteps"]
 
-    if config["model"] == "Discrete2DMidConcatNN":
-        return Discrete2DMidConcatNN(timesteps)
-    elif config["model"] == "Discrete2DNN":
-        return Discrete2DNN(timesteps)
+    if config["model"] == "Discrete2DConcat1Sum":
+        return Discrete2DConcat1Sum(timesteps)
+    elif config["model"] == "Discrete2DSumSuperRes":
+        return Discrete2DSumSuperRes(timesteps)
+    elif config["model"] == "Discrete2DConcat1":
+        return Discrete2DConcat1(timesteps)
+    elif config["model"] == "Discrete2DConcat16":
+        return Discrete2DConcat16(timesteps)
     else:
         raise Exception("Model name unknown.")
