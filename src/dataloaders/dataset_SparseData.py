NNSE, H2, MAPE and model normalization fix for sensor_zscores

src/dataloaders/dataset_SparseData.py
@@ -95,50 +95,55 @@ class Dataset_Sparse(Dataset):
         
     def compute_norm_factors(self, date_max = np.datetime64("2020-01-01"), verbose = True, dict_out = False):
         
-        #Subset wrt Date
         subset_wtd_df = self.wtd_df.loc[pd.IndexSlice[self.wtd_df.index.get_level_values(0) <= date_max,
-                                                       :]] #
-        subset_wtd_df = subset_wtd_df.loc[subset_wtd_df["nan_mask"] == True, :] #compute only for not nan values
+                                                        :]] #
+        #subset_wtd_df = subset_wtd_df.loc[subset_wtd_df["nan_mask"] == True, :] #compute only for not nan values
         subset_weather_xr = self.weather_xr.sel(time = slice(date_max)) #slice include extremes
-        
-        # Compute rasterized NN norm factors 
-        target_means = self.wtd_df[self.target].groupby(level=1).transform('mean').values
-        target_means = target_means.reshape(len(self.wtd_df.index)//len(self.sensor_id_list),
-                                            len(self.sensor_id_list))[0,:] 
-        target_stds = self.wtd_df[self.target].groupby(level=1).transform('std').values
-        target_stds = target_stds.reshape(len(self.wtd_df.index)//len(self.sensor_id_list),
-                                            len(self.sensor_id_list))[0,:]
-        
-        target_means_gpd = gpd.GeoDataFrame({"mean": target_means}, geometry=self.wtd_names.geometry).set_crs(self.wtd_names.crs)
-        target_stds_gpd = gpd.GeoDataFrame({"std": target_stds}, geometry=self.wtd_names.geometry).set_crs(self.wtd_names.crs)
-        
-        bbox = [self.dtm_roi.x.min().values,
-                self.dtm_roi.x.max().values,
-                self.dtm_roi.y.min().values,
-                self.dtm_roi.y.max().values]
+            
+        #Subset wrt Date
+        if self.config["target_norm_type"] == "sensor_zscore":
+            
+            # Compute rasterized NN norm factors 
+            target_means = subset_wtd_df[self.target].groupby(level=1).transform('mean').values
+            target_means = target_means.reshape(len(subset_wtd_df.index)//len(self.sensor_id_list),
+                                                len(self.sensor_id_list))[0,:] 
+            target_stds = subset_wtd_df[self.target].groupby(level=1).transform('std').values
+            target_stds = target_stds.reshape(len(subset_wtd_df.index)//len(self.sensor_id_list),
+                                                len(self.sensor_id_list))[0,:]
+            
+            target_means_gpd = gpd.GeoDataFrame({"mean": target_means}, geometry=self.wtd_names.geometry).set_crs(self.wtd_names.crs)
+            target_stds_gpd = gpd.GeoDataFrame({"std": target_stds}, geometry=self.wtd_names.geometry).set_crs(self.wtd_names.crs)
+            
+            bbox = [self.dtm_roi.x.min().values,
+                    self.dtm_roi.x.max().values,
+                    self.dtm_roi.y.min().values,
+                    self.dtm_roi.y.max().values]
 
-        LON = np.linspace(bbox[0], bbox[1], self.config["upsampling_dim"][2])
-        LAT = np.linspace(bbox[2], bbox[3], self.config["upsampling_dim"][1])[::-1]
-        
-        target_means_raster = self.NN(target_means_gpd, LAT, LON, "mean")
-        target_stds_raster = self.NN(target_stds_gpd, LAT, LON, "std")
-        
-        self.target_means_xr = xarray.DataArray(data = target_means_raster,
-                                coords = dict(
-                                            lat=("lat", LAT),
-                                            lon=("lon", LON),
-                                            ),
-                                dims = ["lat", "lon"]
-                                )
-        
-        self.target_stds_xr = xarray.DataArray(data = target_stds_raster,
-                                coords = dict(
-                                            lat=("lat", LAT),
-                                            lon=("lon", LON),
-                                            ),
-                                dims = ["lat", "lon"]
-                                )
-        
+            LON = np.linspace(bbox[0], bbox[1], self.config["upsampling_dim"][2])
+            LAT = np.linspace(bbox[2], bbox[3], self.config["upsampling_dim"][1])[::-1]
+            
+            target_means_raster = self.NN(target_means_gpd, LAT, LON, "mean")
+            target_stds_raster = self.NN(target_stds_gpd, LAT, LON, "std")
+            
+            self.target_means_xr = xarray.DataArray(data = target_means_raster,
+                                    coords = dict(
+                                                lat=("lat", LAT),
+                                                lon=("lon", LON),
+                                                ),
+                                    dims = ["lat", "lon"]
+                                    )
+            
+            self.target_stds_xr = xarray.DataArray(data = target_stds_raster,
+                                    coords = dict(
+                                                lat=("lat", LAT),
+                                                lon=("lon", LON),
+                                                ),
+                                    dims = ["lat", "lon"]
+                                    )
+        elif self.config["target_norm_type"] == "overall_zscore":
+            
+            target_means = subset_wtd_df[self.target].mean()
+            target_stds = subset_wtd_df[self.target].std()
         # Use true sensor's stats!!!
         # target_means = []
         # target_stds = []
@@ -190,10 +195,14 @@ class Dataset_Sparse(Dataset):
         # Normalizations
         #self.wtd_df[self.target] = (self.wtd_df[self.target] - self.norm_factors["target_mean"])/self.norm_factors["target_std"]
         
-        target_means_extended = np.tile(self.norm_factors["target_means"], len(self.wtd_df.index)//len(self.sensor_id_list))
-        target_stds_extended = np.tile(self.norm_factors["target_stds"], len(self.wtd_df.index)//len(self.sensor_id_list))
-        
-        self.wtd_df[self.target] = (self.wtd_df[self.target] - target_means_extended) / target_stds_extended
+        if self.config["target_norm_type"] == "sensor_zscore":
+            target_norm_means = np.tile(self.norm_factors["target_means"], len(self.wtd_df.index)//len(self.sensor_id_list))
+            target_norm_stds = np.tile(self.norm_factors["target_stds"], len(self.wtd_df.index)//len(self.sensor_id_list))
+        elif self.config["target_norm_type"] == "overall_zscore":
+            target_norm_means = self.norm_factors["target_means"]
+            target_norm_stds = self.norm_factors["target_stds"]
+            
+        self.wtd_df[self.target] = (self.wtd_df[self.target] - target_norm_means) / target_norm_stds
         
             
         self.wtd_df["lat"] = (self.wtd_df["lat"] - self.norm_factors["lat_mean"])/self.norm_factors["lat_std"]

src/loss/losses_2D.py
@@ -20,7 +20,78 @@ def loss_masked_mse(Y_hat, Y, Y_mask, input):
     else:
         return 0.
 
-def loss_masked_nse(Y_hat, Y, Y_mask, input):
+def point_h2(Y_hat, Y, Y_mask, Y_tstep_avail, var_offset = 1e-7):
+    
+    
+    #print(Y_tstep_avail)
+    
+    Y_mean = torch.where(Y_tstep_avail == 0,
+                         0,
+                         torch.sum(Y, dim = 1)/Y_tstep_avail)
+    
+    #print(Y_mean)
+    
+    residuals = torch.sum(((Y_hat - Y)**2)*Y_mask, dim = 1)
+    
+    #print(residuals)
+    Y_variance = torch.where(Y_tstep_avail < 2,
+                         1e-5,
+                         torch.sum(((Y - Y_mean[:,None,:])**2)*Y_mask, dim = 1))
+    
+    #print(Y_variance)
+    h2 = torch.where(Y_tstep_avail < 2,
+                         0,
+                         residuals/Y_variance)
+    
+    #print(h2)
+    
+    # torch.sum(Y, dim = 1)/(Y_tstep_avail + var_offset)
+    # residuals = torch.sum(((Y_hat - Y)**2)*Y_mask, dim = 1)
+    # print(residuals.shape)
+    # Y_variance = torch.sum(((Y - Y_mean[:,None,:])**2)*Y_mask, dim = 1) + var_offset
+    # print(Y_variance.shape)
+    # h2 = residuals/Y_variance
+    
+    return h2
+    
+def loss_masked_h2(Y_hat, Y, Y_mask, input, reduce_mean = True, get_tstep_mask = False):
+    if input == "SparseData_STMoE":
+        if len(Y_hat.size()) < 3:
+            Y_hat = Y_hat.unsqueeze(0) 
+        
+        if torch.sum(Y_mask) != 0:
+            
+            Y_tstep_avail = torch.sum(Y_mask, dim = 1)
+            Y_tstep_avail_mask = (Y_tstep_avail > 1)
+
+            h2 = point_h2(Y_hat, Y, Y_mask, Y_tstep_avail)
+             
+            #print(h2)
+            if reduce_mean is True:
+                h2 = torch.sum(h2[Y_tstep_avail_mask])/torch.sum(Y_tstep_avail_mask)
+            #print(h2)
+        else:
+            print("All NaN! Loss set to 0 ...")
+            h2 = 0
+        
+        if get_tstep_mask is False:
+            return h2
+        else:
+            return [h2, Y_tstep_avail_mask]
+        
+        
+def loss_masked_mape(Y_hat, Y, Y_mask, input):
+    if input == "SparseData_STMoE":
+        if len(Y_hat.size()) < 3:
+            Y_hat = Y_hat.unsqueeze(0) 
+        
+        if torch.sum(Y_mask) != 0:
+            return torch.sum(torch.abs(Y_hat[Y_mask]-Y[Y_mask])/Y[Y_mask]) / torch.sum(Y_mask)
+        else:
+            return 0.
+    
+
+def loss_masked_nse(Y_hat, Y, Y_mask, input, normalized = True):
     
     if input == "ViViT_STMoE":
         if len(Y_hat.size()) < 4:
@@ -29,31 +100,17 @@ def loss_masked_nse(Y_hat, Y, Y_mask, input):
         pass
     
     elif input == "SparseData_STMoE":
-        if len(Y_hat.size()) < 3:
-            Y_hat = Y_hat.unsqueeze(0)      
-        
-        sensor_loss = []
-        
-        avail_sensor_mask = torch.sum(Y_mask, dim = 1)
-        
-        
-        for sensor_idx in range(Y.shape[2]):
             
-            sensor_true = Y[:,:,sensor_idx]
-            sensor_pred = Y_hat[:,:,sensor_idx]
-            sensor_mask = Y_mask[:,:,sensor_idx]
+        h2, Y_tstep_avail_mask = loss_masked_h2(Y_hat, Y, Y_mask, input,
+                                 reduce_mean=False, get_tstep_mask=True)
+        nse = 1 - h2
+        
+        if normalized is True:
+            nse = 1/(2-nse)
             
-            if torch.sum(sensor_mask) != 0:
+        nse = torch.sum(nse[Y_tstep_avail_mask])/torch.sum(Y_tstep_avail_mask)
             
-                sensor_true_filled = torch.where(sensor_mask, sensor_true, sensor_pred)
-                residuals = torch.sum((sensor_pred - sensor_true_filled)**2, dim = 1)
-                Y_mean = torch.mean(sensor_true, dim = 1)
-                Y_variance = torch.nansum((Y_filled))
-                
-                return torch.sum((Y_hat[Y_mask]-Y[Y_mask])**2.0)  / torch.sum(Y_mask)
-                sensor_loss.append()
-            else:
-                sensor_loss.aapend(0)
+        return nse
         
     elif input == "Spatial_STMoE" or input == "Spatial_STMoE_Light":
         if len(Y_hat.size()) < 2:

src/optimizer/load_optimizer.py
@@ -3,16 +3,23 @@ from torch.optim import lr_scheduler
 
 def load_optimizer(config, model):
     
+    if config["loss"] == "nse" or config["loss"] == "nnse":
+        maximize = True
+    else:
+        maximize = False
+    
     if config["optimizer"] == "adam" or config["optimizer"] is None:
         print(f"Optimizer: Adam selected - lr:{config['lr']}")
         return torch.optim.Adam(model.parameters(),
-                                 lr=config['lr'])
+                                 lr=config['lr'],
+                                 maximize=maximize)
         
     elif config["optimizer"] == "adamw":
         print(f"Optimizer: AdamW selected - lr:{config['lr']} - weight_decay: {config['weight_decay']}")
         return torch.optim.AdamW(model.parameters(),
                                  lr=config['lr'],
-                                 weight_decay = config['weight_decay'])
+                                 weight_decay = config['weight_decay'],
+                                 maximize=maximize)
         
     elif config["optimizer"] == "sgd":
         
@@ -35,7 +42,8 @@ def load_optimizer(config, model):
                                lr=config["lr"],
                                momentum=config["momentum"],
                                weight_decay=config["weight_decay"],
-                               nesterov=config["nesterov"])
+                               nesterov=config["nesterov"],
+                               maximize=maximize)
         
         
         