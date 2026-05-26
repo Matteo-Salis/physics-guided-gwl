7c42b4d5ed908cb20f25d0645342e33b79ddde04
Matteo-Salis <salis.matteo98@gmail.com>
2026-05-26 09:24:44 +0200

    Update configs, valid configs, and sh files

---
+++ sh/script_cuda_plot_predictions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks-per-node=1         # 32 tasks per node
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=80000          # memory per node out of 80000MB (80GB)
+#SBATCH --time=05:00:00               # time limits: 1 hour
+#SBATCH --error=/leonardo_scratch/fast/IscrC_DL4STP/physics-guided-gwl/results/logs/plot_predictions.err            # standard error file
+#SBATCH --output=/leonardo_scratch/fast/IscrC_DL4STP/physics-guided-gwl/results/logs/plot_predictions.out           # standard output file
+#SBATCH --account=IscrC_DL4STP       # account name
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --qos=normal             # quality of service
+
+export PYTHONUNBUFFERED=TRUE
+module purge
+
+source /leonardo_scratch/fast/IscrC_DL4STP/.venv/bin/activate
+export WANDB_MODE="offline"
+cd /leonardo_scratch/fast/IscrC_DL4STP/physics-guided-gwl/src
+
+python predictions_MultiPoint.py --config=/leonardo_scratch/fast/IscrC_DL4STP/physics-guided-gwl/config/plot_predictions.json
